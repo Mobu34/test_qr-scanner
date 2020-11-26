2e39eff8b512e7f76a95f3785a25995e729f4a96
Lucas Tidas <Lucas@pc-225.home>
2020-11-26 22:50:58 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import QrReader from "react-qr-scanner";
+
 function App() {
+  const [result, setResult] = useState();
+
+  // console.log(result);
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <QrReader
+        onScan={(data) => {
+          console.log(data);
+        }}
+        onError={(error) => console.log(error)}
+      />
     </div>
   );
 }
